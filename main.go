c587b413c0a72b2a0b957d2e6a657657f649d74c
Odiljon Bokhodirov <odleral77@gmail.com>
2023-10-20 17:02:24 +0500

    fix: thereads command, stop

--- main.go
+++ main.go
@@ -6,149 +6,112 @@ import (
 	"math"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
 	"sync"
-	"time"
+	"syscall"
 )
-
-const (
-	Stop = iota
-	Pause
-	Resume
-	View
-)
-
-type Command struct {
-	t int
-	v int
-}
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println("[main.run]", err)
-	}
-}
-
-func run() error {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered", r)
-		}
-	}()
-	var threads int
-	var totalAns float64
 	var wg sync.WaitGroup
 
-	cmd := make(chan Command, 1)
 	ctx, cancel := context.WithCancel(context.Background())
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-
-	fmt.Print("Threads count: ")
-	_, err := fmt.Scan(&threads)
-	if err != nil {
-		fmt.Println("[main.run] fmt.Scan", err)
-	}
+	defer cancel()
 
 	wg.Add(1)
-	go func(ctx context.Context, wg *sync.WaitGroup) {
-		defer wg.Done()
-
-		num := make(chan float64, 1000)
-		ans := make(chan float64, 1000)
-
-		for i := 0; i < threads; i++ {
-			wg.Add(1)
-			go worker(ctx, num, ans, wg)
-		}
-
-		go generator(ctx, num, cmd)
-		go sum(ans, &totalAns)
-		go enter(ctx, cmd)
-
-	}(ctx, &wg)
-
+	go input(ctx, cancel, &wg)
 	wg.Wait()
-
-	return nil
+	fmt.Println("Gracefull Shutdown")
 }
 
-func worker(ctx context.Context, num chan float64, ans chan float64, wg *sync.WaitGroup) {
+func worker(ctx context.Context, workerNum int, threads int, results chan float64) {
+	var i int
+	var result float64
+
+	run := true
+	fmt.Printf("worker %d started\n", workerNum)
+	for run {
+		select {
+		case <-ctx.Done():
+			results <- result
+			run = false
+		default:
+			i += threads
+
+			result += math.Pow(-1, float64(i-workerNum)) / (2*float64(i-workerNum) + 1)
+		}
+	}
+}
+
+func sum(results chan float64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	total := float64(0)
+
+	for v := range results {
+		total += v
+	}
+
+	fmt.Println("total:", 4*total)
+}
+
+// input ввод в отдельном потоке. Доступные команды threads X, stop, resume X, view X
+func input(ctx context.Context, cancel context.CancelFunc, wg *sync.WaitGroup) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%v\n", r)
+			cancel()
+		}
+	}()
 	defer wg.Done()
 
-	for v := range num {
+	var cmd string
+	var n int
+	var working bool
+	var run = true
+
+	results := make(chan float64, 10)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
 		select {
-		case <-ctx.Done():
-			return
-		default:
-			ans <- math.Pow(-1, v) / (2*v + 1)
+		case <-sigChan:
+			cancel()
+			working = false
+			wg.Add(1)
+			go sum(results, wg)
+			close(results)
+			run = false
 		}
+	}()
 
-	}
-}
-
-func generator(ctx context.Context, num chan float64, cmd chan Command) {
-	i := 0
-	for {
-		i++
-		select {
-		case g := <-cmd:
-			switch g.t {
-			case Pause:
-				time.Sleep(time.Duration(g.v) * time.Second)
-			case Stop:
-				break
-			}
-		case <-ctx.Done():
-			break
-		default:
-			num <- float64(i)
-		}
-	}
-}
-
-func sum(ans chan float64, totalAns *float64) {
-	for v := range ans {
-		*totalAns += v
-	}
-}
-
-func enter(ctx context.Context, cmd chan Command) {
-	for {
-		var scan string
-
-		fmt.Print("Command: ")
-		_, err := fmt.Scan(&scan)
+	for run {
+		_, err := fmt.Scanf("%s %d\n", &cmd, &n)
 		if err != nil {
-			fmt.Println("[main.run] fmt.Scan", err)
-			return
+			if err.Error() != "EOF" {
+				fmt.Println("input error:", err)
+			}
+
+			continue
 		}
 
-		scanSplit := strings.Split(scan, " ")
+		switch cmd {
+		case "t":
+			if working {
+				fmt.Println("Now the program is running, enter stop")
+			}
 
-		switch scanSplit[0] {
+			working = true
+
+			for i := 1; i <= n; i++ {
+				go worker(ctx, i, n, results)
+			}
 		case "stop":
-			fmt.Println("Stop")
-			cmd <- Command{t: Stop}
-			fmt.Println()
-		case "pause":
-			fmt.Println("Pause", scanSplit[1])
-			v, err := strconv.Atoi(scanSplit[1])
-			if err != nil {
-				fmt.Println("Error", err)
-				break
-			}
-			cmd <- Command{t: Pause, v: v}
-		case "resume":
-			fmt.Println("Resume", scanSplit[1])
-			v, err := strconv.Atoi(scanSplit[1])
-			if err != nil {
-				fmt.Println("Error", err)
-				break
-			}
-			cmd <- Command{t: Resume, v: v}
+			cancel()
+			working = false
+			wg.Add(1)
+			go sum(results, wg)
+			close(results)
+			run = false
 		}
 	}
 }
